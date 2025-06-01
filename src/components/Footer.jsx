2ebff80e80677dbb4af625901e59630891fa6cff
Nama Kamu <nizarrarfan@gmail.com>
2025-06-02 00:26:18 +0700

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear(); // Ambil tahun dari waktu sekarang
+
   return (
     <>
       <footer className="mb-0 text-center">
         <div className="d-flex align-items-center justify-content-center pb-5">
           <div className="col-md-6">
-            <p className="mb-3 mb-md-0">Made with ❤️ by {" "}
-              <a  href="https://sahibsingh.dev" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Sahib Singh</a>
+            <p className="mb-3 mb-md-0">
+              &copy; {currentYear} My Website. All rights reserved.
             </p>
-            <a className="text-dark fs-4" href="https://github.com/ssahibsingh" target="_blank" rel="noreferrer">
-              <i className="fa fa-github"></i>
-            </a>
           </div>
         </div>
       </footer>
